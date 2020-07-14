a24b08b480a530b038a0fbbe21468bd2fbfd6445
Liam <bahodirmusayevich@gmail.com>
2020-07-14 19:15:56 +0500

    version 9

--- src/main/java/uz/pdp/dictionary/payload/ReqUser.java
+++ src/main/java/uz/pdp/dictionary/payload/ReqUser.java
@@ -5,18 +5,26 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Data@AllArgsConstructor@NoArgsConstructor
 public class ReqUser {
+    @NotNull
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z1-7]{2,16}$",message = "Wrong entered name")
     private String firstName;
+    @NotNull
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z1-7]{2,16}$",message = "Wrong entered name")
     private String lastName;
+    @NotNull
     @Pattern(regexp = "^[a-zA-Z1-7]{3,12}$",message = "Wrong entered name")
     private String username;
+    @NotNull
     @Pattern(regexp = "((?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,20})", message = "Not allowed password")
     private String password;
+    @NotNull
     @Pattern(regexp = "((?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,20})", message = "Not allowed password")
     private String prePassword;
 }
